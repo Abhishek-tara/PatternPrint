a70d83202e25904e331bbc39599354dca2a3a698
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-05-03 21:38:31 +0530

    add pattern

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -72,13 +72,122 @@
             //    Console.WriteLine();
             //}
 
-            //Number Pyramid
-            int row = 6;
-            for(int i=1;i<=row;i++)
+            //------------------Number Pyramid-----------//
+            //int row = 6;
+            //for(int i=1;i<=row;i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //------------Inverted Number Pyramid--------------//
+            //int row = 6;
+            //for (int i = row; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //---------Number Pyramid increment----------------//
+            //int row = 5;
+            //int num = 1;
+            //for (int i = 1; i <= row; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(num + " ");
+            //        num++;
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //----------- 0-1 Pyramid----------//
+            //1
+            //0 1
+            //1 0 1
+            //int row = 5;
+            //for(int i=1;i<=row;i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        int sum = i + j;
+            //        if(sum % 2 == 0)
+            //        {
+            //            Console.Write(1 + " ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write(0 + " ");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //-------------------Butterfly Pattern---------------//
+            int row = 5;
+            //Upper half
+            for(int i=1;i<=row; i++)
             {
+                //1st part
                 for(int j = 1; j <= i; j++)
                 {
-                    Console.Write(j + " ");
+                    Console.Write("*");
+                }
+                //2nd part
+                int spaces = 2 * (row - i);
+                for (int j = 1; j <= spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+                //3rd part
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            //Lower half
+            for (int i = row; i >= 1; i--)
+            {
+                //1st part
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                //2nd part
+                int spaces = 2 * (row - i);
+                for (int j = 1; j <= spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+                //3rd part
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            //------------Solid Rhombus----------//
+            int num = 5;
+            for(int i = 1; i <= num; i++)
+            {
+                //print spaces
+                for(int j = 1; j <= num - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                //print pattern
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write("*");
                 }
                 Console.WriteLine();
             }
